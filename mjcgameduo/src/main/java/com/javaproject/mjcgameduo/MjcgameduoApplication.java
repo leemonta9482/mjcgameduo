ef971783547feb10d377f4097b1b34ea65b6f70f
leemonta9482 <leemonta9482@gmail.com>
2023-11-06 16:25:11 +0900

    firstCommit

---
+++ mjcgameduo/src/main/java/com/javaproject/mjcgameduo/MjcgameduoApplication.java
@@ -0,0 +1,13 @@
+package com.javaproject.mjcgameduo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MjcgameduoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MjcgameduoApplication.class, args);
+	}
+
+}
