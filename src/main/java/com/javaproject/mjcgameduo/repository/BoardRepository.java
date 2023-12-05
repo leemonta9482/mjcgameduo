be5a3771bda9dbb2c50b51ce46fb75afe3642b58
leemonta9482 <leemonta9482@gmail.com>
2023-12-05 13:27:18 +0900

    12_05_board30MFix

--- src/main/java/com/javaproject/mjcgameduo/repository/BoardRepository.java
+++ src/main/java/com/javaproject/mjcgameduo/repository/BoardRepository.java
@@ -3,11 +3,12 @@ package com.javaproject.mjcgameduo.repository;
 import com.javaproject.mjcgameduo.domain.Board;
 import com.javaproject.mjcgameduo.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
     Long countByWriter(User user);
     void deleteByWriter_Hn(String hn);
-    List<Board> findAllByOrderByCreatedAtDesc();
+
 }
